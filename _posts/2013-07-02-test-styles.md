937812790d857dd9435570bc5bb2dd5f17a564ea
Robert Vinluan <robert.vinluan@gmail.com>
2013-07-02 23:56:26 -0400

    inital commit

---
+++ _posts/2013-07-02-test-styles.md
@@ -0,0 +1,28 @@
+---
+layout: post
+title: "Test Post: Please Ignore"
+subtitle: No, seriously
+hide_subtitle: true
+---
+
+This is a test of my tumblr theme style. It has paragraphs and lists and all kinds of shit.
+
+I’ve been meaning to write about Spelunky for a long time. Probably because it’s without question one of my favorite games ever. In this post I’m going to outline what sets it apart from other games in its genre, and the philosophy it has that I often seek to apply to my own games.
+
+### Here’s a really long section header let’s see what it looks like when it takes up two lines or even more maybe
+
+1. Here's
+2. An
+3. enumerated
+4. list.
+5. and more!
+
+### Another subhead
+
++ whoah there!
++ here's an unordered list
++ with a [link](http://www.google.com) in it!
+
+Spelunky is part of the Roguelike genre, which means it takes elements from the game Rogue, which came out in 1980. Namely: [Permadeath](http://www.google.com), randomly generated levels, and character progression over a “run" (meaning, you play the game in a ‘see how far you can get until you die’-type way). Some people would call it a Roguelike-like, since it doesn’t have some of the major aspects of Rogue, like turn based gameplay, RPG-style stat increases and ASCII graphics.
+
+> You have to look the best to be the best.
---
+++ js/main.js
@@ -0,0 +1,136 @@
+var etc = {
+	palette: [
+		'FFFFFF',
+		'FFFFFF',
+		'FFFFFF',
+		'FFFFFF',
+		'FFFFFF'
+	],
+	paletteTitle: 'undefined',
+	url: '#',
+	firstLoad: true,
+};
+
+/**
+* Converts an RGB color value to HSL. Conversion formula
+* adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+* Assumes r, g, and b are contained in the set [0, 255] and
+* returns h, s, and l in the set [0, 1].
+*
+* @param   Number  r       The red color value
+* @param   Number  g       The green color value
+* @param   Number  b       The blue color value
+* @return  Array           The HSL representation
+*/
+etc.rgbToHsl = function(r, g, b) {
+	r /= 255, g /= 255, b /= 255;
+	var max = Math.max(r, g, b), min = Math.min(r, g, b);
+	var h, s, l = (max + min) / 2;
+
+	if(max == min){
+		h = s = 0; // achromatic
+	}else{
+		var d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		switch(max){
+			case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+			case g: h = (b - r) / d + 2; break;
+			case b: h = (r - g) / d + 4; break;
+		}
+		h /= 6;
+	}
+
+	return [h, s, l];
+}
+
+/*
+Find the index of the largest item in the array.
+This is used to determine which color to use for the background.
+
+As of 11/17 this is no longer in use anywhere,
+but I'm keeping it because it could be one day.
+*/
+etc.findLargest = function(array) {
+	var max_index = -1,
+		max_value = 0;
+	for(var i = 0; i < array.length; i++) {
+		if(array[i] > max_value) {
+			max_value = array[i];
+			max_index = i;
+		}
+	}
+	return max_index;
+}
+
+
+etc.getPalette = function() {
+	$.getJSON("http://www.colourlovers.com/api/palettes/top?jsonCallback=?", {
+		numResults: 1,
+		resultOffset: parseInt(Math.random() * 500, 10),
+		showPaletteWidths: 1
+	}, function(json) {
+		if(json[0].colors.length < 5) {
+			etc.getPalette();
+			return;
+		}
+		etc.palette = json[0].colors;
+		etc.paletteTitle = json[0].title;
+		etc.url = json[0].url;
+		if(etc.firstLoad) {
+			etc.changeColors(etc.palette, etc.paletteTitle, etc.url);
+			etc.firstLoad = false;
+		}
+	});
+}
+
+etc.getPalette();
+
+etc.changeColors = function(palette, title, url) {
+	var flavors,
+		textString;
+		
+	flavors = [
+		$('.colored:nth-child(5n+1)'),
+		$('.colored:nth-child(5n+2)'),
+		$('.colored:nth-child(5n+3)'),
+		$('.colored:nth-child(5n+4)'),
+		$('.colored:nth-child(5n)')
+	];
+	
+	for(var i = 0; i < palette.length; i++) {
+		var lightness = etc.rgbToHsl(
+				parseInt(palette[i].substring(0,2),16),
+				parseInt(palette[i].substring(2,4),16),
+				parseInt(palette[i].substring(4,6),16)
+			)[2];
+		if(lightness >= 0.5) {
+			flavors[i].removeClass('dark').addClass('light');
+		} else {
+			flavors[i].removeClass('light').addClass('dark');
+			$('.color-button').css('color','#'+palette[i]);
+		}
+
+		flavors[i].animate({backgroundColor: '#'+palette[i]}, 200*(i+1));
+	}
+
+	//check to see if the palette title ends with punctuation
+	//if it doesn't, append a period
+	textString = title.match(/[\!\?\.\)]$/) ? title : title + '.';
+
+	$('.palette-title').text(textString).attr('href', url).show();
+
+	etc.getPalette();
+}
+
+etc.init = function() {
+	etc.changeColors(etc.palette, etc.paletteTitle, etc.url);
+}
+
+$(function(){
+	etc.init();
+
+	$('.color-button').click(function(e){
+		e.preventDefault();
+		etc.changeColors(etc.palette, etc.paletteTitle, etc.url);
+	});
+});
